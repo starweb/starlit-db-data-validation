16d5c00667e0d2252a40f13cae1e6f0a5cbbfbd4
Andreas <andreas@starweb.se>
2017-02-18 20:13:37 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,18 @@
+# Utils
+
+[![Build Status](https://travis-ci.org/starweb/starlit-db-data-validation.svg?branch=master)](https://travis-ci.org/starweb/starlit-db-data-validation)
+[![Code Coverage](https://scrutinizer-ci.com/g/starweb/starlit-db-data-validation/badges/coverage.png?b=master)](https://scrutinizer-ci.com/g/starweb/starlit-db-data-validation/?branch=master)
+
+Data validator for starlit database entities.
+
+## Installation
+Add the package as a requirement to your `composer.json`:
+```bash
+$ composer require starlit/db-data-validation
+```
+
+##Requirements
+- Requires PHP 7.0 or above.
+
+##License
+This software is licensed under the BSD 3-Clause License - see the `LICENSE` file for details.
---
+++ src/DbEntityDataValidator.php
@@ -0,0 +1,111 @@
+<?php
+/**
+ * Db data validation.
+ *
+ * @copyright Copyright (c) 2017 Starweb AB
+ * @license   BSD 3-Clause
+ */
+
+namespace Starlit\DbDataValidation;
+
+use Starlit\Db\AbstractDbEntity;
+use Starlit\Validation\Validator;
+use Starlit\Validation\ValidatorTranslatorInterface;
+use Symfony\Component\Translation\TranslatorInterface as SymfonyTranslatorInterface;
+
+/**
+ * Data validator for database entities.
+ */
+class DbEntityDataValidator
+{
+    /**
+     * @var ValidatorTranslatorInterface|SymfonyTranslatorInterface|null
+     */
+    protected $translator;
+
+    public function __construct($translator = null)
+    {
+        $this->translator = $translator;
+    }
+
+    /**
+     * Create a data validator for database entity.
+     *
+     * @param AbstractDbEntity $dbEntity
+     * @return Validator
+     */
+    public function createValidator(AbstractDbEntity $dbEntity, ...$params): Validator
+    {
+        $fieldsRuleProperties = $this->getDbEntityFieldsRuleProperties($dbEntity);
+        $validator = new Validator($fieldsRuleProperties, $this->translator);
+
+        if (($additionalFieldsRuleProperties = $this->getAdditionalFieldsRuleProperties($dbEntity, ...$params))) {
+            $validator->addFieldsRuleProperties($additionalFieldsRuleProperties);
+        }
+
+        return $validator;
+    }
+
+    protected function getDbEntityFieldsRuleProperties(AbstractDbEntity $dbEntity): array
+    {
+        $validRuleProperties = Validator::getValidRuleProperties();
+        $fieldsRuleProperties = [];
+        foreach ($dbEntity->getDbProperties() as $propertyName => $attributes) {
+            // Always validate if validate is not explicitly set to false
+            if (!isset($attributes['validate']) || $attributes['validate'] === true) {
+                $fieldsRuleProperties[$propertyName] = [];
+                foreach ($validRuleProperties as $ruleName) {
+                    if (isset($attributes[$ruleName])) {
+                        $fieldsRuleProperties[$propertyName][$ruleName] = $attributes[$ruleName];
+                    }
+                }
+            }
+        }
+
+        return $fieldsRuleProperties;
+    }
+
+    protected function getAdditionalFieldsRuleProperties(AbstractDbEntity $dbEntity): array
+    {
+        return [];
+    }
+
+    /**
+     * Validate and (if no error messages) set database data.
+     *
+     * @param array  $data The data (e.g. from a form post) to be validated and set
+     * @return array An array with all (if any) of error messages
+     */
+    public function validateAndSet(AbstractDbEntity $dbEntity, array $data): array
+    {
+        // Get extra arguments this method was called with and forward
+        $extraArguments = array_slice(func_get_args(), 2);
+        $validator = $this->createValidator($dbEntity, ...$extraArguments);
+
+        $preProcessedData = $this->preProcessValidationDbData($dbEntity, $data, ...$extraArguments);
+        $errorMessages = $validator->validate($preProcessedData);
+
+        if (empty($errorMessages)) {
+            $this->setValidatedDbData($dbEntity, $validator->getValidatedData());
+        }
+
+        return $errorMessages;
+    }
+
+    protected function preProcessValidationDbData(AbstractDbEntity $dbEntity, array $data): array
+    {
+        return $data;
+    }
+
+    protected function setValidatedDbData(AbstractDbEntity $dbEntity, array $validatedData)
+    {
+        $propertyNames = $dbEntity->getDbProperties();
+        foreach ($validatedData as $propertyName => $value) {
+            if (isset($propertyNames[$propertyName])) {
+                // Call individual setters to enable easy data set overriding
+                $methodName = 'set' . ucfirst($propertyName);
+                $dbEntity->$methodName($validatedData[$propertyName]);
+            }
+        }
+    }
+}
